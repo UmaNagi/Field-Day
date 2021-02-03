cd18a12b07b395e5279914fd529e6f4086755a66
UmaNagi <67808510+UmaNagi@users.noreply.github.com>
2021-02-03 00:14:00 -0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MelonLoader;
+
+namespace Modexample
+{
+    public class Class1 : MelonMod
+    {
+        public float timer;
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+            timer += UnityEngine.Time.deltaTime;
+            if (timer < 12) return;
+            timer = 0;
+            MelonLogger.Msg("Wiping Shaders and Particles");
+            foreach (var item in VRC.SDKBase.VRCPlayerApi.AllPlayers)
+            {
+                foreach (var _item in item.gameObject.GetComponentsInChildren<UnityEngine.MeshRenderer>())
+                    _item.material.shader = UnityEngine.Shader.Find("Standard");
+
+                foreach (var _item in item.gameObject.GetComponentsInChildren<UnityEngine.ParticleSystem>())
+                    UnityEngine.Object.Destroy(_item);
+            }
+        }
+    }
+
+}
